1121dd49df201fe0181b7983a8c15aac051db56e
vitor ambrosio <vitor.ambrosio@aluno.edu.pi.gov.br>
2025-08-26 14:28:21 -0300

    Add files via upload

---
+++ primeiraAtividade.java
@@ -0,0 +1,12 @@
+public class primeiraAtividade {
+
+    public static void main(String[] args) {
+
+
+        double fah = 30;
+        double celsius = (5.0 / 9.0) * (fah - 32);
+        System.out.println("temperatura em celsius: " + celsius);
+
+
+    }
+}
